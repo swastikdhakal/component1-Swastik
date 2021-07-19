ccf6558b346a93d3f1ef8d5abe0ddffaf96dc67a
swastikdhakal <swastikdhakal1999@gmail.com>
2021-07-20 01:00:52 +0545

    component one

---
+++ awe/GameProduct.php
@@ -0,0 +1,33 @@
+<?php
+
+    namespace awe;
+    
+    
+    class GameProduct extends ShopProduct
+    {
+        private $pegi;
+    
+        public function __construct(
+            string $id,
+            string $title,
+            string $firstName,
+            string $mainName,
+            float $price,
+            int $pegi
+        )
+        {
+            parent::__construct(
+                $id,
+                $title,
+                $firstName,
+                $mainName,
+                $price
+            );
+            $this->pegi = $pegi;
+        }
+    
+        public function getPegiRating()
+        {
+            return $this->pegi;
+        }
+    }
